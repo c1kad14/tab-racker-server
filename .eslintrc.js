600cc79b38ff0255c6b95934117ad12d0ec6e177
deech <iurii.bobrykov@gmail.com>
2019-02-26 12:00:00 +1300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+	'env': {
+    "node": true,
+		'es6': true
+	},
+	'extends': 'eslint:recommended',
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly'
+	},
+	'parserOptions': {
+		'ecmaVersion': 2018,
+		'sourceType': 'module'
+	},
+	'rules': {
+	  'no-console':'off',
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
